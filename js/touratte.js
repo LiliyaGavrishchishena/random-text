c68e9f7884c75d8f7af5b1ffc8a08e5199398d3f
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-07-22 22:23:33 +0300

    start of project

---
+++ js/touratte.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const authorsList = [
+  {
+    _id: 1,
+    name: 'Todd'
+  },
+  {
+    _id: 3,
+    name: 'Rob'
+  },
+  {
+    _id: 3,
+    name: 'Sevil'
+  }
+];
+
+console.log(authorsList);
